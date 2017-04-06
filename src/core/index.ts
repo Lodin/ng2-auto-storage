a5c924257a34a9c838a2acb1f4d36cb62729536b
Vlad Rindevich <rindevich.vs@gmail.com>
2017-04-06 08:12:19 +0300

    feat(core): improve core index

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,1 +1,5 @@
+export {ActionRegistry} from './action-registry';
 export {GlobalSettings} from './global-settings';
+export {SerializationRule} from './serialization-rule';
+export {StorageOperator} from './storage-operator';
+export {WebStorage} from './web-storage';
